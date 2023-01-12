c770d55e863f73a5631ec4e3a9db4b6dbdb6a152
Bruno Souza <souza.brunoha@gmail.com>
2023-01-12 19:12:35 -0300

    feat: add tailwindCSS module

--- app.vue
+++ app.vue
@@ -1,8 +1,7 @@
 <template>
-  <h1>{{ helloWorld }}</h1>
+  <h1 class="text-center text-red-500 text-3xl">{{ helloWorld }}</h1>
 </template>
 
 <script setup lang="ts">
   const helloWorld = 'Hello World'
-  const teste = ''
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
   vite: {
     plugins: [eslintPlugin()],
   },
